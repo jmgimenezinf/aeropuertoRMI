2fd138c4fe285a9a5db7bde3074649e4d273a9c6
martin <martin@martin.martin>
2017-08-19 19:35:00 -0300

    Turnos v.0.9

--- src/ControladorAereo/Turnos.java
+++ src/ControladorAereo/Turnos.java
@@ -6,4 +6,23 @@ import Avion.Avion;
 
 public class Turnos {
 	private Queue<Avion> turnos = new PriorityQueue<Avion>();
+	
+	
+	public Avion proximoTurno() {
+		return this.getTurnos().poll();
+	}
+	
+	public void asignarTurno(Avion avion) {
+		this.getTurnos().add(avion);
+		avion.setTurno(this.getTurnos().size());	
+	}
+
+	private Queue<Avion> getTurnos() {
+		return turnos;
+	}
+
+	private void setTurnos(Queue<Avion> turnos) {
+		this.turnos = turnos;
+	}
+	
 }
