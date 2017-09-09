95d54dedadb55eb0f129375f7ef0034835bcde51
martin gimenez <jm.gimenez.inf@gmail.com>
2017-09-09 03:44:54 -0300

    checkout a master

--- src/source/Configuracion.java
+++ src/source/Configuracion.java
@@ -1,13 +1,9 @@
 package source;
 
 public class Configuracion {
-	public static String ipServidor = "192.168.0.103"; 
+	public static String ipServidor = "127.0.1.1"; 
 	public static Integer puertoServidor= 7000;
-<<<<<<< HEAD
-
-=======
 	public static String ipCliente = "127.0.1.1"; 
 	public static long maxDeriva = 1000;
-	
->>>>>>> master
+
 }
