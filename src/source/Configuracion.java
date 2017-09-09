99c234466d9583a528da6c6fc511de53ce769303
Macarena Solange Quiroga <msinf.quiroga@gmail.com>
2017-09-09 03:19:18 -0300

    UML

--- src/source/Configuracion.java
+++ src/source/Configuracion.java
@@ -1,7 +1,7 @@
 package source;
 
 public class Configuracion {
-	public static String ipServidor = "127.0.1.1"; 
+	public static String ipServidor = "192.168.0.103"; 
 	public static Integer puertoServidor= 7000;
-	public static String ipCliente = "127.0.1.1"; 
+	public static String ipCliente = "192.168.0.105"; 
 }
