82610801299606462629d88999c4c7e97776f7f4
martin gimenez <jm.gimenez.inf@gmail.com>
2017-09-09 03:30:26 -0300

    Checkout macarena

--- src/source/Configuracion.java
+++ src/source/Configuracion.java
@@ -4,6 +4,6 @@ public class Configuracion {
 	public static String ipServidor = "127.0.1.1"; 
 	public static Integer puertoServidor= 7000;
 	public static String ipCliente = "127.0.1.1"; 
-	public static float maxDeriva = (float) 0.02;
+	public static long maxDeriva = 1000;
 	
 }
